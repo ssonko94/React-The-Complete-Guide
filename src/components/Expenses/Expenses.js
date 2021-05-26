29123d2a57d7196846b9d3088c698663a2b164bd
ssonko94 <ssonkojimmy@gmail.com>
2021-05-26 19:45:08 +0300

    add a filter method to render items depending on the selected year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,25 +1,22 @@
 import React, { useState } from "react";
 
 import ExpenseFilter from "../ExpenseFilter/ExpenseFilter";
-import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
+import ExpensesList from "./ExpensesList";
 export default function Expenses(props) {
   const [year, setYear] = useState("2020");
   const onSelectExepenseYear = (year) => {
     setYear(year);
   };
 
+  const filteredExpenses = props.expenses.filter((expense) => {
+    return expense.date.getFullYear().toString() === year;
+  });
+
   return (
     <div className="expenses">
       <ExpenseFilter selected={year} onSelectChange={onSelectExepenseYear} />
-      {props.expenses.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        ></ExpenseItem>
-      ))}
+      <ExpensesList items={filteredExpenses} />
     </div>
   );
 }
