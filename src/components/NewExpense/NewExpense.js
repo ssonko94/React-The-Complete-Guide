af08c37f49b0a62be11fc45d22f0f20b420e0c87
ssonko94 <ssonkojimmy@gmail.com>
2021-05-26 19:49:48 +0300

    use a state variable to toggle between showing and closing the form depending on the button click and submit button

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
   const submitExpenseDataHandler = (submitedExpenseData) => {
     const expenseData = {
       ...submitedExpenseData,
@@ -9,11 +10,28 @@ const NewExpense = (props) => {
     };
 
     props.addExpense(expenseData);
+    setIsEditing(false);
+  };
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
   };
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSubmitExpenseData={submitExpenseDataHandler} />
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSubmitExpenseData={submitExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
     </div>
   );
 };
